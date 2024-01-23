0a5e5bf5b954e4510fd9768749b261512aef8270
Gion <69393137+gionsunday@users.noreply.github.com>
2024-01-23 18:03:26 +0530

    Add files via upload

--- routes/newTransactionsRoutes.js
+++ routes/newTransactionsRoutes.js
@@ -5,12 +5,16 @@ const {
     getAllTransactions,
     createSuport,
     createTransaction,
-    
+    updateTransaction,
+    getSingleTransaction,
 } = require('../controllers/newtransactions')
 
 router.route('/').post(createTransaction)
 
 router.route('/createsupport').post(createSuport)
+router.route('/updatetransaction').post(updateTransaction)
+
+router.route('/getOnetransaction/:transactionID').get(getSingleTransaction)
 router.route('/:id').get(getAllTransactions)
 
 module.exports = router

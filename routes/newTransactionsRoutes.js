c04bb8d38b515a0cbcb5dd22d3bc76422854a959
Gion <69393137+gionsunday@users.noreply.github.com>
2024-01-29 18:13:23 +0530

    Update newTransactionsRoutes.js

--- routes/newTransactionsRoutes.js
+++ routes/newTransactionsRoutes.js
@@ -14,7 +14,7 @@ router.route('/').post(createTransaction)
 router.route('/createsupport').post(createSuport)
 router.route('/updatetransaction').post(updateTransaction)
 
-router.route('/getOnetransaction/:transactionID').get(getSingleTransaction)
+router.route('/getOnetransaction/').post(getSingleTransaction)
 router.route('/:id').get(getAllTransactions)
 
 module.exports = router

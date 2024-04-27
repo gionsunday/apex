22f266e35ee5416b0d3bb2c6d93ee75035d4d1c3
Gion <69393137+gionsunday@users.noreply.github.com>
2024-04-27 07:57:19 +0000

    refferal link

--- controllers/referral.js
+++ controllers/referral.js
@@ -2,7 +2,7 @@ var refferal_code = "";
 const referral = (req, res) => {
   console.log(req.params);
   refferal_code = req.params.referalcode;
-  return res.redirect("https://apexcorporatefinanceltd.com/accounts/user");
+  return res.redirect("http://apexcorporatefinanceltd.com/accounts/users/user");
 
   //   res.redirect("<script> window.location="https://apex.onrender.com/home"<script/>")
 };

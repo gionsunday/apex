6e7b8b81235db5e4a2f318e6e94141c41d08b672
gionsunday <richardadaji@gmail.com>
2025-12-02 11:13:33 +0100

    update

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,21 +5,21 @@ const generateToken = (userId, res, expires = "15d") => {
     expiresIn: expires,
   });
 
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: false, // false on localhost
-    path: "/",
-    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-    sameSite: "lax", // works locally
-  });
+  // res.cookie("jwt", token, {
+  //   httpOnly: true,
+  //   secure: false, // false on localhost
+  //   path: "/",
+  //   maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+  //   sameSite: "lax", // works locally
+  // });
 
-  //   res.cookie("jwt", token, {
-  //     httpOnly: true, // more secure
-  //     secure: true ,      // Only over HTTPS
-  //     path:"/",
-  //     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-  //     sameSite: 'None',   // Allow cross-origin
-  //   });
+    res.cookie("jwt", token, {
+      httpOnly: true, // more secure
+      secure: true ,      // Only over HTTPS
+      path:"/",
+      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+      sameSite: 'None',   // Allow cross-origin
+    });
 
   return token;
 };
